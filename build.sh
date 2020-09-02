7def8ca77b8f0e0cc1f2111d6024598dcd520f03
Semih Celek <cybermoon1999@gmail.com>
2020-09-02 21:33:43 +0300

    add bash script for building public folder for nginx-server

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ -d public ]] #checks if there's any public folder exists
+then
+   echo "Removing Existing PublicFolder..."
+   rm -rf public
+   echo "Building Hugo Site..."
+   hugo -D
+else
+  echo "Building Hugo Site..."
+  hugo -D
+fi
+
+echo "Copy public to nginx folder"
+rm -rf /usr/share/nginx/html/public && cp $(pwd)/public /usr/share/nginx/html
